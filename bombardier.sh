5a75f986ee626303a48e418cfed269c58a38ce35
Lo Gap <gaplotech@gmail.com>
2018-03-11 18:30:39 +0000

    re-run all n1-highcpu-4 with a stronger client to prevent client bottle-neck

--- bombardier.sh
+++ bombardier.sh
@@ -4,7 +4,7 @@ mkdir -p $2;
 sudo ~/go/bin/bombardier -c 125 -n 100000 -l $1 > "$2/warmup1.txt";
 sudo ~/go/bin/bombardier -c 250 -n 100000 -l $1 > "$2/warmup2.txt";
 
-for value in 125 250 500 1000 2000
+for value in 125 250 500 1000 2000 3000
 do
   sudo ~/go/bin/bombardier -c $value -n 500000 -l $1 > "$2/c$value.txt";
 done
