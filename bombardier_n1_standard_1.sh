735f53ce4dc24030d174a317c59a7da8bcea41fe
Lo Gap <gaplotech@gmail.com>
2018-03-12 16:02:13 +0000

    increase request number, remove 2ms 5ms test

--- bombardier_n1_standard_1.sh
+++ bombardier_n1_standard_1.sh
@@ -6,7 +6,7 @@ sudo ~/go/bin/bombardier -c 250 -n 20000 -l $1 > "$2/warmup2.txt";
 
 for value in 50 125 250 500 1000 2000
 do
-  sudo ~/go/bin/bombardier -c $value -n 100000 -l $1 > "$2/c$value.txt";
+  sudo ~/go/bin/bombardier -c $value -n 500000 -l $1 > "$2/c$value.txt";
 done
 
 
--- run.sh
+++ run.sh
@@ -9,6 +9,6 @@ rm -rf "$result_path$4-5ms";
 rm -rf "$result_path$4-10ms";
 
 sh $1 $url "$result_path$4-0ms";
-sh $1 "$url/2ms" "$result_path$4-2ms";
-sh $1 "$url/5ms" "$result_path$4-5ms";
+#sh $1 "$url/2ms" "$result_path$4-2ms";
+#sh $1 "$url/5ms" "$result_path$4-5ms";
 sh $1 "$url/10ms" "$result_path$4-10ms";
